2d432c7e68591101f2ce73c3bd7066c257c6054e
SarahAnberlin <sarahanberlin@outlook.com>
2024-09-25 11:51:48 +0800

    ?

--- generate_depth_for_all.py
+++ generate_depth_for_all.py
@@ -5,21 +5,6 @@ from depth_anything_v2.dpt import DepthAnythingV2
 from eva_metrics import delta1_acc_np, delta2_acc_np, delta3_acc_np, abs_relative_difference_np, \
     threshold_percentage_np, mse_np
 
-DEVICE = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
-
-model_configs = {
-    'vits': {'encoder': 'vits', 'features': 64, 'out_channels': [48, 96, 192, 384]},
-    'vitb': {'encoder': 'vitb', 'features': 128, 'out_channels': [96, 192, 384, 768]},
-    'vitl': {'encoder': 'vitl', 'features': 256, 'out_channels': [256, 512, 1024, 1024]},
-    'vitg': {'encoder': 'vitg', 'features': 384, 'out_channels': [1536, 1536, 1536, 1536]}
-}
-
-encoder = 'vitl'  # or 'vits', 'vitb', 'vitg'
-
-model = DepthAnythingV2(**model_configs[encoder])
-model.load_state_dict(torch.load(f'checkpoints/depth_anything_v2_{encoder}.pth', map_location='cpu'))
-model = model.to(DEVICE).eval()
-
 
 def process_image(image_path, model):
     raw_img = cv2.imread(image_path)
@@ -60,12 +45,29 @@ def process_folder(image_dir, model, gt_depth, noise_type):
 
 # 请你把对于每个文件夹，生成他对应的depth文件，dir_name_depth，basename同名，然后把
 if __name__ == '__main__':
+
+    DEVICE = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
+
+    model_configs = {
+        'vits': {'encoder': 'vits', 'features': 64, 'out_channels': [48, 96, 192, 384]},
+        'vitb': {'encoder': 'vitb', 'features': 128, 'out_channels': [96, 192, 384, 768]},
+        'vitl': {'encoder': 'vitl', 'features': 256, 'out_channels': [256, 512, 1024, 1024]},
+        'vitg': {'encoder': 'vitg', 'features': 384, 'out_channels': [1536, 1536, 1536, 1536]}
+    }
+
+    encoder = 'vitl'  # or 'vits', 'vitb', 'vitg'
+
+    model = DepthAnythingV2(**model_configs[encoder])
+    model.load_state_dict(torch.load(f'checkpoints/depth_anything_v2_{encoder}.pth', map_location='cpu'))
+    model = model.to(DEVICE).eval()
+
     data_root = "/dataset/vfayezzhang/dataset/LOLdataset/"
     for root, dir, files in os.walk(data_root):
         for file in files:
             if 'depth' in root or 'depth' in file or not (file.endswith('.jpg') or file.endswith('.png')):
                 continue
             image_path = os.path.join(root, file)
+            print(f"Processing {image_path}")
             depth = process_image(image_path, model)
             depth_dir_name = os.path.join(root, f'{dir}_depth')
             os.makedirs(depth_dir_name, exist_ok=True)
