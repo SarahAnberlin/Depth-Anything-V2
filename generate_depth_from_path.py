584a9313aa9c07a0e96d192c8c7f1a7dda62207d
SarahAnberlin <sarahanberlin@outlook.com>
2024-09-30 15:38:25 +0800

    ?

--- generate_depth_from_path.py
+++ generate_depth_from_path.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
         'vits': {'encoder': 'vits', 'features': 64, 'out_channels': [48, 96, 192, 384]},
         'vitb': {'encoder': 'vitb', 'features': 128, 'out_channels': [96, 192, 384, 768]},
         'vitl': {'encoder': 'vitl', 'features': 256, 'out_channels': [256, 512, 1024, 1024]},
-        'vitg': {'encoder': 'vitg', 'features': 384, 'out_channels': [1536, 1536, 1536, 1536]}
+        # 'vitg': {'encoder': 'vitg', 'features': 384, 'out_channels': [1536, 1536, 1536, 1536]}
     }
 
     data_root = r'/dataset/vfayezzhang/dataset/SIDD/test/gt'
