5c2037c04a1744f16a728d1e26697aa510b63b31
SarahAnberlin <sarahanberlin@outlook.com>
2024-12-12 16:49:18 +0800

    123

--- marigold_depth.py
+++ marigold_depth.py
@@ -1,5 +1,6 @@
 import diffusers
 import torch
+from torchvision.utils import save_image
 
 pipe = diffusers.MarigoldDepthPipeline.from_pretrained(
     "prs-eth/marigold-depth-lcm-v1-0", variant="fp16", torch_dtype=torch.float16
@@ -7,7 +8,8 @@ pipe = diffusers.MarigoldDepthPipeline.from_pretrained(
 
 image = diffusers.utils.load_image("https://marigoldmonodepth.github.io/images/einstein.jpg")
 depth = pipe(image).prediction
-
+depth = depth.squeeze()
+save_image(depth, "depth.png")
 print(f"Depth shape: {depth.shape}")
 print(f"Depth dtype: {depth.dtype}")
 print(f"Depth range: {depth.min(), depth.max()}")
