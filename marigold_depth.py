7e53848bd91b42c6ac66f5618935511911e25908
SarahAnberlin <sarahanberlin@outlook.com>
2024-12-12 16:50:40 +0800

    123

--- marigold_depth.py
+++ marigold_depth.py
@@ -7,7 +7,7 @@ pipe = diffusers.MarigoldDepthPipeline.from_pretrained(
 ).to("cuda")
 
 image = diffusers.utils.load_image("https://marigoldmonodepth.github.io/images/einstein.jpg")
-depth = pipe(image).prediction
+depth = pipe(image, output_type='pt')
 depth = depth.squeeze()
 save_image(depth, "depth.png")
 print(f"Depth shape: {depth.shape}")
