d872842f16e062c5d5634e3fe9d91d619b173328
SarahAnberlin <sarahanberlin@outlook.com>
2024-12-12 15:40:47 +0800

    123

--- generate_depth.py
+++ generate_depth.py
@@ -80,7 +80,7 @@ def worker(rank, world_size, encoder, model_configs, dataset, save_root):
             print(f"Depth prediction shape: {depth.shape}")
             predict_depth_np = depth.squeeze().cpu().numpy()
             depth_gt_np = depth_gt.squeeze().cpu().numpy()
-
+            save_single_fig(predict_depth_np, save_root, idx)
             # save_fig(image_numpy, predict_depth_np, depth_gt_np, save_root, idx)
 
 
