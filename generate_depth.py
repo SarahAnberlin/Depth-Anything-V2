af3b857db36fd4684b558fc7eb5aa1b69eced7b5
SarahAnberlin <sarahanberlin@outlook.com>
2024-12-13 15:57:34 +0800

    123

--- generate_depth.py
+++ generate_depth.py
@@ -49,6 +49,7 @@ def save_fig(image, predict_depth_vis, depth_gt_vis, save_root, id):
 
 def save_single_fig(predict_depth, save_root, id):
     predict_depth = (predict_depth - np.min(predict_depth)) / (np.max(predict_depth) - np.min(predict_depth))
+    predict_depth = (1 - predict_depth)
     predict_depth = predict_depth * 255.0
     predict_depth = predict_depth.astype(np.uint8)
     save_path = os.path.join(save_root, f"{id}.png")
