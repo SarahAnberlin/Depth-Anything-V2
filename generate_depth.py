c291c72f2994e41cd7905d9d390f680a92717667
SarahAnberlin <sarahanberlin@outlook.com>
2024-12-13 14:56:41 +0800

    123

--- generate_depth.py
+++ generate_depth.py
@@ -110,7 +110,7 @@ if __name__ == '__main__':
         'vitg': {'encoder': 'vitg', 'features': 384, 'out_channels': [1536, 1536, 1536, 1536]}
     }
     # dataset_name = 'Sintel'
-    dataset_name = 'NYUv2'
+    dataset_name = 'Sintel'
     dataset = get_dataset(dataset_name)
     save_root = '/dataset/vfayezzhang/test/depth-pro/infer/vis/dav2/'
     save_root = os.path.join(save_root, dataset_name)
