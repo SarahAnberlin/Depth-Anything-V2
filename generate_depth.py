e6be3fcdfa1020f203202ceaadc29d2706548b73
SarahAnberlin <sarahanberlin@outlook.com>
2025-01-07 09:56:57 +0800

    123

--- generate_depth.py
+++ generate_depth.py
@@ -110,11 +110,11 @@ def worker(rank, world_size, encoder, model_configs, dataset, save_root):
         for idx, data in enumerate(dataset):
             cnt += 1
             image = data[0]
-            image = image.to(device)
 
             for resolution in resolutions:
                 image = transforms.Resize((resolution, resolution))(image)
                 image = image.unsqueeze(0).to(device)
+
                 # image = torchvision.transforms.Resize((1540, 1540))(image)
                 h, w = image.shape[-2:]
                 # pad_h = 14 - ((h + 14) % 14)
